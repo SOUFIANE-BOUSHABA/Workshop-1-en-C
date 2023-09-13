c87b3fee129eeb658439fc78d0219e2d7ca24b3f
souf139 <soufianboushaba12@gmail.com>
2023-09-13 15:24:57 +0100

    this commit for the challenge 3

---
+++ challenge3/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//a et b sont deux entiers saisies au clavier, calculez et 
+//affichez a+b, a-b, a*b, a/b, a%b en format décimal, et en soignant l’interface homme/machine. 
+//(a/b donne le quotient de la division, a%b donne le reste de la division)
+
+
+int main(int argc, char *argv[]) {
+    int a;
+    int b;
+    int sum , less , multiple , division ;
+    
+    printf("give me  number one : ");
+    scanf("%d", &a);
+    
+    printf("give me  number two : ");
+    scanf("%d", &b);
+    
+    sum = a+b;
+    less = a-b;
+    multiple = a*b;
+    division = a/b;
+   
+    printf(" the sum is : %d\n", sum);
+    printf(" the less is : %d\n", less);
+    printf(" the multiple is : %d\n", multiple);
+    printf(" the division is : %d\n", division);
+}
+
